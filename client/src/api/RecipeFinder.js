b9988048d682c510c873966ca9828e9529558399
jason <jfadelli@gmail.com>
2022-03-11 14:38:35 -0800

    v1.0.0

--- client/src/api/RecipeFinder.js
+++ client/src/api/RecipeFinder.js
@@ -1,7 +1,18 @@
 import axios from 'axios'
 
+
+// NODE_ENV = 'development'
+// NODE_ENV = 'production'
+
+// if we are in production, baseURL = /api/v1/recipes
+// else, baseURL = http://localhost:3001/api/v1/recipes
+
+const baseURL = process.env.NODE_ENV === 'production' ? '/api/v1/recipes' : 'http://localhost:3001/api/v1/recipes'
+
+// const baseURL = "http://localhost:54/api/v1/recipes" //Lan
+// const baseURL:"http://72.197.66.232:52/api/v1/recipes" // WAN
+// const baseURL:"http://localhost:52/api/v1/recipes" // LOCAL
+
 export default axios.create({
-    baseURL:"http://192.168.0.3:52/api/v1/recipes" // LAN 
-    // baseURL:"http://72.197.66.232:52/api/v1/recipes" // WAN
-    // baseURL:"http://localhost:52/api/v1/recipes" // LOCAL
+    baseURL: baseURL
 })
